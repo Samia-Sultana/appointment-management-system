6be552daedae4f6a1563c6c387c3a7ea63d243f6
Samia Sultana <samia.cse.ewu@gmail.com>
2023-02-02 14:47:03 +0600

    feat: admin dashboard theme integrated

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::group(['middleware' => 'auth'], function(){
+    Route::group([
+        'prefix' => 'admin',
+        'as' => 'admin.'
+    ], function(){
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::get('/dashboard', function(){
+            return view('admin.dashboard');
+        })->name('dashboard');
+    });
+
 });
 
 require __DIR__.'/auth.php';
